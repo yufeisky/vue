f22e5294380802a31935de77c071bdf13f891e0f
Bin 林 <binlin@MacBook-Pro-2.local>
2018-09-12 09:28:52 +0800

    给src/platforms/web/runtime/patch.js添加备注

--- src/platforms/web/runtime/patch.js
+++ src/platforms/web/runtime/patch.js
@@ -7,6 +7,14 @@ import platformModules from 'web/runtime/modules/index'
 
 // the directive module should be applied last, after all
 // built-in modules have been applied.
+/**
+ * yufeisky
+ * 把平台的modules跟基础的modules合并； 
+ */
 const modules = platformModules.concat(baseModules)
 
+/**
+ * yufeisky
+ * 把{ nodeOps, modules })传入createPatchFunction函数，经过一系列处理返回一个新的函数patch
+ */
 export const patch: Function = createPatchFunction({ nodeOps, modules })
